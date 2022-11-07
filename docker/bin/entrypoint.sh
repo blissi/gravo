f374948776b7382d62f561f5635641e3d1e8cdaa
Steven Weiß <info@code-factory.de>
2022-11-07 14:45:32 +0100

    Fixed startup error.

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -7,12 +7,7 @@ HASSIO_OPTIONSFILE=/data/options.json
 if [ -f ${HASSIO_OPTIONSFILE} ]; then
     API=$(grep -o '"api": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
     echo "Using api endpoint: ${API}"
-
-    if [ ! -f "${API}" ]; then
-        echo "api not found. Please set up this option in the addon configuration."
-    else
-        gravo -api "${API}"
-    fi
+    gravo -api "${API}"
 else
     gravo
 fi
